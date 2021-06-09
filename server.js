573f5a6bf0c82c5ed5fbdfd0fa40ea58700e05a5
herirudini <75158630+herirudini@users.noreply.github.com>
2021-06-09 12:56:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ next()
 }
 const express = require("express");
 const app = express();
-app.use(requireHTTPS)
+//app.use(requireHTTPS)
 app.use(express.static('./dist/e-commerce'));
 app.get('/*', function(req,res){
 res.sendFile('index.html', {root: 'dist/e-commerce'})
