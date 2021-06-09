4f906006586d2638295469ea53309a60b6db556a
herirudini <75158630+herirudini@users.noreply.github.com>
2021-06-09 12:42:14 +0700

    debug

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const path = require("path");
+function requireHTTPS(req, res, next) {
+if (!req.secure && req.get('x-forwareded-proto') !== 'https') {
+return res.redirect('https://' + req.get('host') + req.url)
+}
+next()
+}
 const express = require("express");
 const app = express();
-app.use(express.static('dist/build'));
+app.use(requireHTTPS)
+app.use(express.static('./dist/e-commerce'));
 app.get('/*', function(req,res){
-res.sendFile(path.join('dist/build', 'index.html'))
+res.sendFile('index.html', {root: 'dist/e-commerce'})
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
